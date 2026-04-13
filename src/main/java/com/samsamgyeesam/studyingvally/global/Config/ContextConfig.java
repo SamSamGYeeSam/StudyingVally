198d429ae32f00b7fa665d2bac8e1aab8b64bc7f
JindoKim <kjd020322@gmail.com>
2026-04-13 17:29:29 +0900

    init

---
+++ src/main/java/com/samsamgyeesam/studyingvally/global/Config/ContextConfig.java
@@ -0,0 +1,21 @@
+package com.samsamgyeesam.studyingvally.global.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.samsamgyeesam.studyingvally")
+public class ContextConfig {
+
+   @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+        return modelMapper;
+    }
+
+}
---
+++ src/main/java/com/samsamgyeesam/studyingvally/global/Config/StudyingVallyApplication.java
@@ -0,0 +1,13 @@
+package com.samsamgyeesam.studyingvally.global.Config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingVallyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyingVallyApplication.class, args);
+    }
+
+}
