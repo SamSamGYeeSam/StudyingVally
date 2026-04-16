c49a00f03b6cc2da9f65ad4b9070fdfc60d61090
JISEOKBEOM <obs2da78@gmail.com>
2026-04-16 12:33:20 +0900

    [기능구현] 고객센터 관리 페이지 기능 구현 * 로그인 기능 가져오기 위한 머지 진행

--- src/main/java/com/samsamgyeesam/studyingvally/domain/admin/service/AdminCallCenterService.java
+++ src/main/java/com/samsamgyeesam/studyingvally/domain/admin/service/AdminCallCenterService.java
@@ -6,5 +6,28 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AdminCallCenterService {}
+public class AdminCallCenterService {
 
+    public List<AdminCallCenterMenuDTO> findAllCallCenterItems() {
+        return List.of(
+                new AdminCallCenterMenuDTO(
+                        1,
+                        "공지사항",
+                        "공지사항 목록이 뜨며 공지를 추가할 수 있음",
+                        "/admin/callcenter/notice"
+                ),
+                new AdminCallCenterMenuDTO(
+                        2,
+                        "문의함",
+                        "문의함을 누르면 문의 목록을 확인할 수 있음",
+                        "/admin/callcenter/contact"
+                ),
+                new AdminCallCenterMenuDTO(
+                        3,
+                        "신고함",
+                        "공지사항 신고 내역 전체 확인 가능",
+                        "/admin/callcenter/report"
+                )
+        );
+    }
+}
--- src/main/java/com/samsamgyeesam/studyingvally/global/Config/SecurityConfig.java
+++ src/main/java/com/samsamgyeesam/studyingvally/global/Config/SecurityConfig.java
@@ -177,7 +177,7 @@ public class SecurityConfig {
                         .loginProcessingUrl("/auth/login")
                         .usernameParameter("loginId")
                         .passwordParameter("password")
-                        .successHandler(authSuccessHandler)
+//                        .successHandler(authSuccessHandler)
                         .failureUrl("/auth/login?error=true")
                 )
                 .formLogin(form -> form.disable());
