4f3cf639c80fe3c35f63658fa66788e2f405260b
sooyoung0927 <sooyoung040927@gmail.com>
2026-04-21 13:12:19 +0900

    [화면 구현] 임수영 - 관리자 페이지 css 작업

--- src/main/java/com/samsamgyeesam/studyingvally/domain/admin/adminusercare/dto/AdminUserDetailResponseDTO.java
+++ src/main/java/com/samsamgyeesam/studyingvally/domain/admin/adminusercare/dto/AdminUserDetailResponseDTO.java
@@ -17,6 +17,7 @@ public class AdminUserDetailResponseDTO {
     private String userName;
     private String userNickname;
     private String userPhoneNumber;
+    private String userGender;
 
     private String userRole;
     private String userStatus;
